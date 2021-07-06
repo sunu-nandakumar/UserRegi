c2b99d978662302a621ea1357c5725eda8515041
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-07-06 21:57:12 +0530

    adding the last name uc2

--- UserReg.java
+++ UserReg.java
@@ -14,15 +14,31 @@ public class UserReg {
 		Matcher match = patt.matcher(firstname);
 		return match.matches();
 	}
+	public static boolean isLastName(String lastname) {
+		String regex = "^[A-Z]{1}[a-z]{2,}";
+		Pattern patt2 = Pattern.compile(regex);
+		if (lastname == null) {
+			return false;
+		}
+		Matcher match2 = patt2.matcher(lastname);
+		return match2.matches();
+	}
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter firstname:");
 		String firstname = input.nextLine();
+		System.out.println("Enter lastname:");
+		String lastname = input.nextLine();
 		if (isFirstName(firstname) == true) {
 			System.out.println("Firstname is Correct");
 		} else {
 			System.out.println("Firstname is Incorrect");
+		}
+		if (isLastName(lastname) == true) {
+			System.out.println("Lastname is Correct");
+		} else {
+			System.out.println("Lastname is Incorrect");
 		}
 	}
 
